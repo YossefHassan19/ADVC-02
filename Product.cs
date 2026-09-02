cc17c4185c2f5196e7aa7dd4d66aa39572ff8676
YossefHassan19 <yossef.hassan678@gmail.com>
2026-09-02 21:36:01 +0300

    Task 01 : Smart Product Search

---
+++ Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADVC_02
+{
+    internal class Product
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+        public static List<Product> SearchProducts(List<Product> products, Func<Product, bool> factory)
+        {
+            List<Product> Listproducts = new List<Product>();
+
+            foreach (Product product in products)
+            {
+                if (factory(product))
+                    Listproducts.Add(product);
+            }
+            return Listproducts;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,60 @@
         static void Main(string[] args)
         {
 
+            #region Task 01 : Smart Product Search
+
+
+            //Task 01 : Smart Product Search
+
+            //List<Product> catalog = new()       
+            //{
+            //    new Product { Id=1, Name="Laptop", Category="Electronics", Price=1200, Stock=10 },
+            //    new Product { Id=2, Name="Phone", Category="Electronics", Price=800, Stock=25 },
+            //    new Product { Id=3, Name="T-Shirt", Category="Clothing", Price=30, Stock=100 },
+            //    new Product { Id=4, Name="Jeans", Category="Clothing", Price=60, Stock=50 },
+            //    new Product { Id=5, Name="Chocolate", Category="Food", Price=5, Stock=200 },
+            //    new Product { Id=6, Name="Coffee Beans", Category="Food", Price=15, Stock=80 },
+            //    new Product { Id=7, Name="C# Book", Category="Books", Price=45, Stock=30 },
+            //    new Product { Id=8, Name="Novel", Category="Books", Price=20, Stock=60 },
+            //    new Product { Id=9, Name="Headphones", Category="Electronics", Price=150, Stock=40 },
+            //    new Product { Id=10, Name="Jacket", Category="Clothing", Price=120, Stock=15 }
+            //};
+
+            //Console.WriteLine("---Electronics---");
+            //List<Product> electronicsFilter =  Product.SearchProducts(catalog, product => product.Category == "Electronics");
+
+            //foreach (Product product in electronicsFilter)
+            //{
+            //    Console.WriteLine($"{product.Name} - ${product.Price} (Stock:{product.Stock})");
+            //}
+
+            //Console.WriteLine("\n---Under $50---");
+            //List<Product> cheapProducts = Product.SearchProducts(catalog, product => product.Price < 50);
+
+            //foreach (Product product in cheapProducts)
+            //{
+            //    Console.WriteLine($"{product.Name} - ${product.Price} (Stock:{product.Stock})");
+            //}
+
+            //Console.WriteLine("\n---In Stock---");
+            //List<Product> inStockProducts = Product.SearchProducts(catalog, product => product.Stock > 0);
+
+            //foreach (Product product in inStockProducts)
+            //{
+            //    Console.WriteLine($"{product.Name} - ${product.Price} (Stock:{product.Stock})");
+            //}
+
+            //Console.WriteLine("\n---Clothing Under $100---");
+            //List<Product> clothingUnder100 = Product.SearchProducts(catalog, product => product.Category == "Clothing" && product.Price < 100);
+
+            //foreach (Product product in clothingUnder100)
+            //{
+            //    Console.WriteLine($"{product.Name} - ${product.Price} (Stock:{product.Stock})");
+            //}
+
+
+            #endregion
+
         }
     }
 }
